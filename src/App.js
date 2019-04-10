4ffd7fcc4c1c17792f630fbca93f781c29637215
Sergei <staroduber@yandex.ru>
2019-04-10 20:56:33 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Tasks from "./components/Tasks.js";
+import "./App.scss";
+import Icon1 from "./icons/icon1.png";
+import Icon2 from "./icons/ic-help-outline-24-px-copy-3.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <header
+        className="navbar navbar-expand navbar-dark flex-column flex-md-row bd-navbar"
+        style={{ background: "gray" }}
+      >
+        <a className="navbar-brand mr-0 mr-md-2"> taskon </a>
+        <div className="navbar-nav-scroll">
+          <ul className="navbar-nav bd-navbar-nav flex-row">
+            <li className="navbar-nav bd-navbar-nav flex-row">
+              <a className="nav-link active"> Задания</a>
+            </li>
+            <li className="navbar-nav bd-navbar-nav flex-row">
+              <a className="nav-link"> Финансы</a>
+            </li>
+            <li className="navbar-nav bd-navbar-nav flex-row">
+              <a className="nav-link"> Компания</a>
+            </li>
+            <li className="navbar-nav bd-navbar-nav flex-row">
+              <a className="nav-link"> Статистика</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="navbar-nav flex-row ml-md-auto d-none d-md-flex">
+          <li className="nav-item">
+            <a className="nav-link p-2"> Баланс: 204 012 Р</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link p-2">
+              {" "}
+              <img src={Icon1} />
+            </a>
+          </li>
+
+          <li className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Сергей Стародубов
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a className="dropdown-item" href="#">
+                Логин
+              </a>
+              <a className="dropdown-item" href="#">
+                Настройки
+              </a>
+              <div className="dropdown-divider" />
+              <a className="dropdown-item" href="#">
+                Выйти
+              </a>
+            </div>
+          </li>
+
+          <li className="nav-item">
+            <a className="nav-link p-2">
+              {" "}
+              <img src={Icon2} />
+            </a>
+          </li>
+        </ul>
+      </header>
     );
   }
 }
---
+++ src/components/Tasks.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Tasks extends React.Component {
+  render () {
+    return (
+      <>
+        hello world11
+      </>
+    )
+  }
+}
+
+export default Tasks;
